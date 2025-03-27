98217d6a2ffeab6f2553df49243820fca52404b7
João Pedro Magalhães <joaom00.dev@gmail.com>
2025-03-27 16:54:08 -0300

    refactor: remove tailwind and use css instead

--- src/components/blockicon.tsx
+++ src/components/blockicon.tsx
@@ -1,45 +1,25 @@
 import { ComponentProps } from "react";
-import { tv, VariantProps } from "tailwind-variants";
 import { cn } from "../utils/cn";
 
 const BASE_URL =
   "https://blockicon-bucket.s3.us-west-1.amazonaws.com/blockicon/pre-alpha";
 
-const iconVariants = tv({
-  base: "flex items-center justify-center flex-shrink-0 object-contain overflow-clip",
-  variants: {
-    shape: {
-      square: "rounded-md",
-      circle: "rounded-full",
-    },
-    size: {
-      sm: "size-4",
-      md: "size-6",
-      lg: "size-8",
-      xl: "size-10",
-    },
-  },
-  defaultVariants: {
-    shape: "circle",
-    size: "md",
-  },
-});
-
 type ImageProps = ComponentProps<"img">;
-type IconVariants = VariantProps<typeof iconVariants>;
 type IconTheme = "original" | "dark" | "light";
 
-interface BlockIconProps extends Omit<ImageProps, "src">, IconVariants {
+interface BlockIconProps extends Omit<ImageProps, "src"> {
   asset: string | number;
   theme?: IconTheme;
+  shape?: "circle" | "square";
+  size?: "sm" | "md" | "lg" | "xl";
 }
 
 export const BlockIcon = (props: BlockIconProps) => {
   const {
     asset,
     theme = "original",
-    shape,
-    size,
+    shape = "circle",
+    size = "md",
     className,
     ...imgProps
   } = props;
@@ -51,7 +31,7 @@ export const BlockIcon = (props: BlockIconProps) => {
       data-shape={shape}
       data-size={size}
       data-theme={theme}
-      className={cn(iconVariants({ shape, size }), className)}
+      className={cn("blockicon", className)}
       {...imgProps}
     />
   );
