75015ca8bd3e71bdcfe195ec616d66e091806ac9
João Pedro Magalhães <joaom00.dev@gmail.com>
2025-03-30 19:50:12 -0300

    refactor: adjust project to publish package

---
+++ packages/react/src/aliases/index.ts
@@ -0,0 +1,7 @@
+import { NETWORKS_ALIAS } from "./networks";
+import { TOKENS_ALIAS } from "./tokens";
+
+export const ALIASES = {
+  network: NETWORKS_ALIAS,
+  token: TOKENS_ALIAS,
+} as const;
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,65 @@
+import { ComponentProps } from "react";
+import { ALIASES } from "./aliases/index";
+
+const BASE_URL = "https://d3v6728skxiwy6.cloudfront.net/blockicon";
+
+type ImageProps = Omit<ComponentProps<"img">, "src">;
+
+type Aliases = typeof ALIASES;
+
+type Networks = keyof Aliases["network"];
+type Tokens = keyof Aliases["token"];
+
+type AssetProps = {
+  category?: "token";
+  asset: Tokens;
+};
+
+type NetworkProps = {
+  category?: "network";
+  chain: Networks;
+};
+
+type BlockIconProps = ImageProps & {
+  shape?: "circle" | "square";
+  size?: "sm" | "md" | "lg" | "xl";
+} & (AssetProps | NetworkProps);
+
+export const BlockIcon = (props: BlockIconProps) => {
+  let _props = {} as Omit<BlockIconProps, "chain" | "asset">;
+
+  const alias = (() => {
+    if (props.category === "network") {
+      const { chain, ...restProps } = props;
+      _props = restProps;
+      return ALIASES[props.category][chain];
+    }
+    if (props.category === "token") {
+      const { asset, ...restProps } = props;
+      _props = restProps;
+      return ALIASES[props.category][asset];
+    }
+
+    throw new Error("Provide a valid category");
+  })();
+
+  const {
+    category = "token",
+    shape = "circle",
+    size = "md",
+    className,
+    ...imgProps
+  } = _props;
+
+  return (
+    <img
+      src={`${BASE_URL}/${category}/${alias}.svg`}
+      alt={`${alias} icon`}
+      data-shape={shape}
+      data-size={size}
+      data-category={category}
+      className={`blockicon ${className}`}
+      {...imgProps}
+    />
+  );
+};
