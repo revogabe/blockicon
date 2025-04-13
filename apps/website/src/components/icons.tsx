0db3435d14077939102c7acb666afd9032f3f26a
João Pedro Magalhães <joaom00.dev@gmail.com>
2025-04-12 23:32:34 -0300

    feat: add react virtuoso

--- apps/website/src/components/icons.tsx
+++ apps/website/src/components/icons.tsx
@@ -1,203 +1,113 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
-import Image from "next/image";
-import { Button } from "./ui/button";
-import { Command } from "./ui/command";
-import { frameworks, packagesManager, installCommand } from "@/mock";
-import { useVirtualizer } from "@tanstack/react-virtual";
-import { motion, Transition, delay } from "motion/react";
+import { ChangeEvent, forwardRef, useState } from "react";
 import { cn } from "@/utils/cn";
 import { ALIASES } from "core";
-import { BlockIcon, type BlockIconProps } from "blockicon";
+import { Components, VirtuosoGrid } from "react-virtuoso";
+import { matchSorter } from "match-sorter";
+import * as motion from "motion/react-client";
+
+const sizes = ["sm", "md", "lg", "xl"] as const;
+const categories = ["token", "network"] as const;
+
+type Size = (typeof sizes)[number];
+type Category = (typeof categories)[number];
+
+const uniqueNetworks = Array.from(new Set(Object.values(ALIASES.network)));
+const uniqueTokens = Array.from(new Set(Object.values(ALIASES.token)));
 
 export const Icons = () => {
-  const uniqueNetworks = useMemo(
-    () => Array.from(new Set(Object.values(ALIASES.network))),
-    [],
-  );
-  const uniqueTokens = useMemo(
-    () => Array.from(new Set(Object.values(ALIASES.token))),
-    [],
-  );
-
   const [search, setSearch] = useState("");
-  const [filteredBySearch, setFilteredBySearch] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<Category>("token");
+  const [selectedSize, setSelectedSize] = useState<Size>("xl");
 
-  const parentRef = useRef(null);
-  const categoryRef = useRef<HTMLDivElement>(null);
-  const sizeRef = useRef<HTMLDivElement>(null);
-  const [iconType, setIconType] = useState<"token" | "network">("token");
-  const [size, setSize] = useState<"sm" | "md" | "lg" | "xl">("xl");
+  const [filteredBySearch, setFilteredBySearch] = useState<string[]>(
+    selectedCategory === "token" ? uniqueTokens : uniqueNetworks,
+  );
 
-  const [underlineStyle, setUnderlineStyle] = useState({
-    width: 0,
-    left: 0,
-  });
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const dataToFilter =
+      selectedCategory === "token" ? uniqueTokens : uniqueNetworks;
 
-  const [underlineSizeStyle, setUnderlineSizeStyle] = useState({
-    width: 0,
-    left: 0,
-  });
+    const matches = matchSorter(dataToFilter, value);
+    setFilteredBySearch(matches);
+    setSearch(value);
+  };
 
-  const rowVirtualizer = useVirtualizer({
-    count: filteredBySearch.length,
-    getScrollElement: () => parentRef.current,
-    estimateSize: () => 60,
-  });
-
-  useEffect(() => {
-    const dataToFilter = iconType === "token" ? uniqueTokens : uniqueNetworks;
-
-    const filtered = dataToFilter.filter((item) =>
-      item.toLowerCase().includes(search.toLowerCase()),
-    );
-
-    setFilteredBySearch(filtered);
-  }, [search, iconType, uniqueNetworks, uniqueTokens]);
-
-  useEffect(() => {
-    if (!sizeRef.current) return;
-
-    const buttons = sizeRef.current.querySelectorAll("button");
-    const activeIndex =
-      size === "sm" ? 0 : size === "md" ? 1 : size === "lg" ? 2 : 3;
-    const activeButton = buttons[activeIndex];
-
-    if (activeButton) {
-      const rect = activeButton.getBoundingClientRect();
-      const containerRect = sizeRef.current.getBoundingClientRect();
-
-      setUnderlineSizeStyle({
-        width: rect.width,
-        left: rect.left - containerRect.left,
-      });
-    }
-  }, [size]);
-
-  useEffect(() => {
-    if (!categoryRef.current) return;
-
-    const buttons = categoryRef.current.querySelectorAll("button");
-    const activeIndex = iconType === "token" ? 0 : 1;
-    const activeButton = buttons[activeIndex];
-
-    if (activeButton) {
-      const rect = activeButton.getBoundingClientRect();
-      const containerRect = categoryRef.current.getBoundingClientRect();
-
-      setUnderlineStyle({
-        width: rect.width,
-        left: rect.left - containerRect.left,
-      });
-    }
-  }, [iconType]);
+  const handleCategoryClick = (category: Category) => {
+    const dataToFilter = category === "token" ? uniqueTokens : uniqueNetworks;
+    const matches = matchSorter(dataToFilter, search);
+    setSelectedCategory(category);
+    setFilteredBySearch(matches);
+  };
 
   return (
-    <div className="relative container mx-auto border-x border-zinc-900 flex flex-col items-center justify-between w-full pb-20">
+    <div className="relative container mx-auto border-x border-zinc-900 w-full pb-20">
       <div className="flex w-full items-center justify-between border-b border-zinc-900">
         <input
           type="text"
           placeholder="Search..."
           className="h-16 w-full px-6 outline-none focus:bg-zinc-900/50 border-r border-zinc-900 flex-1"
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleSearchChange}
         />
-        <div
-          ref={categoryRef}
-          className="relative flex border-r border-zinc-900"
-        >
-          <div
-            className="h-px absolute bg-white bottom-0 transition-all duration-300 ease-out"
-            style={{ ...underlineStyle }}
-          />
-
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-[127px] hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              iconType === "token" && "text-white",
-            )}
-            onClick={() => setIconType("token")}
-          >
-            Tokens
-          </button>
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-[127px] hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              iconType === "network" && "text-white",
-            )}
-            onClick={() => setIconType("network")}
-          >
-            Networks
-          </button>
+        <div className="flex border-r border-zinc-900">
+          {categories.map((category) => (
+            <button
+              key={category}
+              className={cn(
+                "relative text-lg text-zinc-500 h-16 w-[127px] hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800 capitalize",
+                selectedCategory === "token" && "text-white",
+              )}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}s
+              {selectedCategory === category && (
+                <motion.div
+                  layoutId="category-underline"
+                  id="category-underline"
+                  className="absolute h-px bottom-0 inset-x-0 bg-white"
+                />
+              )}
+            </button>
+          ))}
         </div>
-        <div ref={sizeRef} className="relative flex">
-          <div
-            className="h-px absolute bg-white bottom-0 transition-all duration-300 ease-out"
-            style={{ ...underlineSizeStyle }}
-          />
-
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-16 hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              size === "sm" && "text-white",
-            )}
-            onClick={() => setSize("sm")}
-          >
-            sm
-          </button>
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-16 hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              size === "md" && "text-white",
-            )}
-            onClick={() => setSize("md")}
-          >
-            md
-          </button>
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-16 hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              size === "lg" && "text-white",
-            )}
-            onClick={() => setSize("lg")}
-          >
-            lg
-          </button>
-          <button
-            className={cn(
-              "text-lg text-zinc-500 h-16 w-16 hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
-              size === "xl" && "text-white",
-            )}
-            onClick={() => setSize("xl")}
-          >
-            xl
-          </button>
+        <div className="flex">
+          {sizes.map((size) => (
+            <button
+              key={size}
+              className={cn(
+                "relative text-lg text-zinc-500 h-16 w-16 hover:bg-zinc-900/50 duration-300 ease-out outline-none focus:bg-zinc-900/50 border-b border-transparent focus:border-zinc-800",
+                selectedSize === size && "text-white",
+              )}
+              onClick={() => setSelectedSize(size)}
+            >
+              {size}
+              {selectedSize === size && (
+                <motion.div
+                  layoutId="size-underline"
+                  id="size-underline"
+                  className="absolute h-px bottom-0 inset-x-0 bg-white"
+                />
+              )}
+            </button>
+          ))}
         </div>
       </div>
-      <div
-        ref={parentRef}
-        className="h-[800px] overflow-auto w-full flex flex-wrap"
-      >
-        <div
-          style={{
-            height: rowVirtualizer.getTotalSize(),
-            position: "relative",
-          }}
-        >
-          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-            const icon = filteredBySearch[virtualRow.index];
-            return (
-              <IconItem
-                key={icon}
-                icon={icon}
-                iconType={iconType}
-                size={size}
-                virtualRow={virtualRow}
-              />
-            );
-          })}
-        </div>
-      </div>
+
+      <VirtuosoGrid
+        style={{ height: 500 }}
+        data={filteredBySearch}
+        components={{ List }}
+        itemContent={(_index, icon) => (
+          <IconItem
+            key={icon}
+            icon={icon}
+            iconType="network"
+            size={selectedSize}
+          />
+        )}
+      />
     </div>
   );
 };
@@ -205,25 +115,13 @@ export const Icons = () => {
 type IconItemProps = {
   icon: string;
   iconType: string;
-  size: "sm" | "md" | "lg" | "xl";
-  virtualRow: { start: number };
+  size: Size;
 };
 
-const IconItem = ({ icon, iconType, size, virtualRow }: IconItemProps) => {
-  const [isVisible, setIsVisible] = useState(true);
-  if (!isVisible) return null;
-
+const IconItem = ({ icon, iconType, size, ...props }: IconItemProps) => {
   return (
     <div
-      key={icon}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: 400,
-        transform: `translateY(${virtualRow.start}px)`,
-      }}
+      {...props}
       className="flex flex-col items-center px-4 py-2 border border-zinc-900 justify-center gap-4 w-full"
     >
       <img
@@ -235,7 +133,6 @@ const IconItem = ({ icon, iconType, size, virtualRow }: IconItemProps) => {
           "size-10": size === "lg",
           "size-14": size === "xl",
         })}
-        onError={() => setIsVisible(false)}
       />
       <span className={cn("text-lg text-white")}>
         {icon
@@ -247,3 +144,14 @@ const IconItem = ({ icon, iconType, size, virtualRow }: IconItemProps) => {
     </div>
   );
 };
+
+const List: Components["List"] = forwardRef((props, ref) => {
+  const { children, ...divProps } = props;
+  return (
+    <div {...divProps} ref={ref} className="grid grid-cols-4">
+      {children}
+    </div>
+  );
+});
+
+List.displayName = "List";
